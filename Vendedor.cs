066fcf2e6c56a9faa44708d02ee4410b1cc3b6a8
sercho80 <46472925+sercho80@users.noreply.github.com>
2020-10-26 18:17:31 +0100

    Se vuelve a dejar la rama como antes

--- Vendedor.cs
+++ Vendedor.cs
@@ -7,14 +7,11 @@ namespace fabricantevendedor
     {
         private Almacen _a;
         private Thread _t;
-        private int _periodicidad;
-        private int _cantidad;
+        private Random _rnd = new Random();
         
-        public Vendedor(Almacen a, int periodicidad, int cantidad)
+        public Vendedor(Almacen a)
         {
             this._a = a;
-            this._periodicidad = periodicidad;
-            this._cantidad = cantidad;
         }
 
         public void Vende()
@@ -30,9 +27,11 @@ namespace fabricantevendedor
 
         private void _Accion()
         {
-            for (int i = 0; i < this._cantidad; i++)
+            int ms;
+            for (int i = 0; i < 10; i++)
             {
-                Thread.Sleep(_periodicidad);
+                ms = _rnd.Next(1000, 1500);
+                Thread.Sleep(ms);
                 _a.Sacar();
             }
         }
